5c2e1104bda3a09921b229dc46563888d1615cd6
prokopis3 <prokopis123@gmail.com>
2024-12-02 19:39:07 +0200

    fix(karma config, angular.json): fix karma.config, angular.json

--- karma.conf.js
+++ karma.conf.js
@@ -3,7 +3,14 @@ process.env.CHROME_BIN = require('puppeteer').executablePath();
 module.exports = function (config) {
     config.set({
         frameworks: ['jasmine', '@angular-devkit/build-angular'],
-        browsers: ['ChromeHeadless'], // Use Headless mode for automation
-        singleRun: true,              // Ensure it runs once and exits
+        files: [],
+        plugins: [
+            require('karma-jasmine'),
+            require('karma-chrome-launcher'),
+            require('@angular-devkit/build-angular/plugins/karma'),
+        ],
+        browsers: ['ChromeHeadless'],
+        singleRun: true,
+        reporters: ['progress'],
     });
 };
